6e305c1f1b429261c8689f0383fdbca36c414f21
오민석 <22854000+MinseokOh@users.noreply.github.com>
2022-04-21 17:23:26 +0900

    fix: deleted unsued scripts

--- scripts/stop_kafka.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#kill zookeeper
-echo "===> stop zookeeper service"
-ps ax | grep 'zookeeper' | awk '{print $1}' | xargs kill
-
-echo "===> stop kafka service"
-ps ax | grep 'kafka-server' | awk '{print $1}' | xargs kill
